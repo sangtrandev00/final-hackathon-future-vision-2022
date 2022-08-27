62eae1de5cddc64253bc45ebcff1465141e67cd8
sangtrandev00 <nhatsang0101@gmail.com>
2022-08-27 22:04:02 +0700

    add important project

--- src/demo_project_frontend/src/components/ImportantDonation/index.jsx
+++ src/demo_project_frontend/src/components/ImportantDonation/index.jsx
@@ -1,30 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import ImportantImg_1 from '../../../assets/img/img-ctrinh/ctrinh-quantrong/hinh-1.jpeg';
+import { demo_project_backend } from '../../../../declarations/demo_project_backend/index';
 ImportantDonation.propTypes = {};
 
 function ImportantDonation(props) {
+  const [importantProject, setImportantProject] = useState({});
+  const { currentProgress, setCurrentProgress } = useState();
+  const LinkUrlToDetailFundPage = `/fund-detail-page?name=${importantProject.ProjectID}`;
+  console.log(importantProject);
+  useEffect(() => {
+    const getImportantProject = async () => {
+      const importantProject = await demo_project_backend.view_fund_project('1');
+      console.log('importantProject', importantProject[0]);
+      setImportantProject(importantProject[0]);
+
+      const currentProgress = importantProject[0].CurrentMoney / importantProject[0].TargetMoneyy;
+      setCurrentProgress(currentProgress);
+    };
+
+    getImportantProject();
+  }, []);
+
   return (
     <div>
       <section class="donate_section">
         <div class="container">
           <div class="row">
             <div class="col-md-7 for-padding">
-              <img src={ImportantImg_1} alt="" />
+              <img src={importantProject.ImgUrl} alt="important image" />
             </div>
             <div class="col-md-5 for-padding">
               <h4>DỰ ÁN QUAN TRỌNG</h4>
-              <h3>GIÚP TRẺ MỒ CÔI TẠI MÁI ẤM:</h3>
-              <p>
-                Mái ấm Tín Thác là nơi đã thu nhận, nuôi dưỡng, chăm sóc những đứa trẻ mồ côi,
-                khuyết tật. Đồng thời cũng là vòng tay lớn yêu thương, che chở cho những đứa bé đã
-                bị bỏ rơi trong suốt 10 năm qua. Tại nơi đây, hàng trăm đứa trẻ đã thực sự có được
-                một mái ấm thân thương như ruột thịt đúng nghĩa. Đây cũng chính là nguồn cảm hứng,
-                tạo động lực cho các bạn sinh viên Cao đẳng FPT Polytechnic của team 28s Event thực
-                hiện dự án mang tên “Ban mai giữa phong ba”
-              </p>
+              <h3>{importantProject.ProjectName}</h3>
+              <p>{importantProject.ShortDesc}</p>
               <div class="progress-text">
-                <p class="progress-top">50%</p>
+                <p class="progress-top">{currentProgress}%</p>
                 <div class="progress">
                   <div
                     class="progress-bar"
@@ -32,14 +44,14 @@ function ImportantDonation(props) {
                     aria-valuenow="70"
                     aria-valuemin="0"
                     aria-valuemax="100"
-                    style={{ width: '50%' }}
+                    style={{ width: `${currentProgress}%` }}
                   ></div>
                 </div>
-                <p class="progress-left">Đã góp: 5.000.000</p>
-                <p class="progress-right">Mục tiêu: 10.000.000</p>
+                <p class="progress-left">Đã góp: {importantProject.CurrentMoney}</p>
+                <p class="progress-right">Mục tiêu: {importantProject.TargetMoneyy}</p>
               </div>
               <h2>
-                <a href="#">ĐÓNG GÓP NGAY</a>
+                <Link to={LinkUrlToDetailFundPage}>ĐÓNG GÓP NGAY</Link>
               </h2>
             </div>
           </div>
